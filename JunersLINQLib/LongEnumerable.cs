8721bc4e74ca81a3816227b47e9ffdc3fe9b70f1
juner <juner@juner.net>
2019-01-24 11:40:55 +0900

    Chunkのテストが通っていなかった為、修正。 Chunkのlong対応

---
+++ JunersLINQLib/LongEnumerable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Juners.Linq {
+    public static class LongEnumerable {
+        public static IEnumerable<long> Range(long start, long count)
+        {
+            var max = start + count - 1;
+            if (count < 0 || max > long.MaxValue)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            return LongRangeIterator(start, count);
+        }
+        static IEnumerable<long> LongRangeIterator(long start, long count)
+        {
+            for (var i = 0L; i < count; i++)
+                yield return start + i;
+        }
+        public static IEnumerable<TResult> Repeat<TResult>(TResult element, long count)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            return LongRepeatIterator(element, count);
+        }
+        static IEnumerable<TResult> LongRepeatIterator<TResult>(TResult element, long count)
+        {
+            for (var i = 0L; i < count; i++)
+                yield return element;
+        }
+        public static IEnumerable<TSource> Take<TSource>(this IEnumerable<TSource> source, long count)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            return TakeIterator(source, count);
+        }
+        static IEnumerable<TSource> TakeIterator<TSource>(IEnumerable<TSource> source, long count)
+        {
+            if (count > 0) {
+                foreach (var element in source) {
+                    yield return element;
+                    if (--count == 0)
+                        break;
+                }
+            }
+        }
+        public static IEnumerable<TSource> Skip<TSource>(this IEnumerable<TSource> source, long count)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            return SkipIterator(source, count);
+        }
+        static IEnumerable<TSource> SkipIterator<TSource>(IEnumerable<TSource> source, long count)
+        {
+            using (var e = source.GetEnumerator()) {
+                while (count > 0 && e.MoveNext())
+                    count--;
+                if (count <= 0) {
+                    while (e.MoveNext())
+                        yield return e.Current;
+                }
+            }
+        }
+    }
+}
--- JunersLINQLibTests/ChunkExtensionsTests.cs
+++ JunersLINQLibTests/ChunkExtensionsTests.cs
@@ -1,10 +1,11 @@
 ﻿using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Juners.Linq.Tests {
     [TestClass]
-    public class ChunkTests {
+    public class ChunkExtensionsTests {
         [TestMethod]
         public void ChunkTest()
         {
@@ -19,7 +20,21 @@ namespace Juners.Linq.Tests {
             var Array3 = Array1.Chunk(3).ToArray();
             Trace.WriteLine($"{nameof(Array3)}:[{string.Join(", ", Array3.Select(v => $"[{string.Join(", ", v)}]"))}]");
             Assert.AreEqual($"[{string.Join(", ", ExpectArray3.Select(v => $"[{string.Join(", ", v)}]"))}]", $"[{string.Join(", ", Array3.Select(v => $"[{string.Join(", ", v)}]"))}]");
-
+            var ExpectArray4 = new[] { new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, }};
+            Trace.WriteLine($"{nameof(ExpectArray4)}:[{string.Join(", ", ExpectArray4.Select(v => $"[{string.Join(", ", v)}]"))}]");
+            var Array4 = Array1.Chunk(10).ToArray();
+            Trace.WriteLine($"{nameof(Array4)}:[{string.Join(", ", Array4.Select(v => $"[{string.Join(", ", v)}]"))}]");
+            Assert.AreEqual($"[{string.Join(", ", ExpectArray4.Select(v => $"[{string.Join(", ", v)}]"))}]", $"[{string.Join(", ", Array4.Select(v => $"[{string.Join(", ", v)}]"))}]");
+        }
+        [TestMethod]
+        public async Task ChunkがlongをサポートするかのTest()
+        {
+            await Task.Run(() => {
+                var LongArray1 = Enumerable.Range(0, int.MaxValue).Loop().Take(int.MaxValue + 1L);
+                foreach (var value in LongArray1.Chunk(10).Select(v => v.ToArray()))
+                    ;
+            });
+            ;
         }
     }
 }
